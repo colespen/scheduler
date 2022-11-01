8d039c4cddca74be4552878f654ccc785997a99a
colespen <spencerlewiscole@gmail.com>
2022-11-01 16:47:57 -0400

    set state in Application parent component and pass to children components

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { useState } from "react";
 import "components/Application.scss";
 
 import DayList from "./DayList";
@@ -23,6 +23,9 @@ const days = [
 ];
 
 export default function Application(props) {
+
+  const [day, setDay] = useState("Monday");
+
   return (
     <main className="layout">
 
@@ -36,8 +39,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={days}
-            day={"Monday"}
-            setDay={day => console.log(day)} 
+            day={day}
+            setDay={setDay} 
             //3 PROPS passed to DayList!!
           />
         </nav>
--- src/components/DayList.js
+++ src/components/DayList.js
@@ -7,8 +7,9 @@ export default function DayList(props) {
   const dayListItems = props.days.map(el => (
     <DayListItem
       key={el.id} name={el.name} spots={el.spots}
-      selected={el.name === props.day} setDay={props.setDay} //DayList accepts setDay as prop..?
+      selected={el.name === props.day} setDay={props.setDay} 
       //selected=(obj.name === props.day)
+      //DayList accepts setDay as prop and passes it to DayListItem 
     />
   ));
   return (
