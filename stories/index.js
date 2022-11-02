f3a3fa3de990b9cb29eace4b31c9872e34990cce
colespen <spencerlewiscole@gmail.com>
2022-11-02 15:15:20 -0400

    begin building form component

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -168,9 +169,25 @@ storiesOf("Appointment", module)
     />
   )
   .add("Status", () => <Status message="Deleting" />)
-  .add("Error", () => 
-  <Error 
-  message="Could not delete appointment." 
-  onClose={action("onClose")}
-  />
+  .add("Error", () =>
+    <Error
+      message="Could not delete appointment."
+      onClose={action("onClose")}
+    />
+  )
+  .add("Form Create", () =>
+    <Form
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  )
+  .add("Form Edit", () =>
+    <Form
+      student="Archie Cohen"
+      interviewer={3}
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
   )
