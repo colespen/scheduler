71b9f8006ff74867a0161bad26e65b93bdbdf345
colespen <spencerlewiscole@gmail.com>
2022-11-02 14:33:40 -0400

    all Appointment children components created

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,9 @@ import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 storiesOf("Button", module)
   .addParameters({
@@ -156,4 +159,18 @@ storiesOf("Appointment", module)
       onEdit={action("onEdit")}
       onDelete={action("onDelete")}
     />
-  );
+  )
+  .add("Confirm", () =>
+    <Confirm
+      message="Delete the appointment?"
+      onCancel={action("onCancel")}
+      onConfirm={action("onConfirm")}
+    />
+  )
+  .add("Status", () => <Status message="Deleting" />)
+  .add("Error", () => 
+  <Error 
+  message="Could not delete appointment." 
+  onClose={action("onClose")}
+  />
+  )
